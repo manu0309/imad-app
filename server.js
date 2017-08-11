ac2b6bf89227ca77609002aa7c44db91ba3cd5d1
manu0309 <guptamanu530@gmail.com>
2017-08-11 22:27:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ var articleOne = {
         </p>`
 };
 
-function creationTemplate (data) {
+function createTemplate (data) {
     var title = data.title;
     var date = data.date;
     var heading = data.heading;
