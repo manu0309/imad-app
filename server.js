f50f4b62c1f565a13e4bcfb18510918bd884aeb5
manu0309 <guptamanu530@gmail.com>
2017-08-11 22:41:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(morgan('combined'));
 
 var articles = {
-' article-one' :{
+'article-one' :{
     title: 'Article One I Manu Gutpa',
     heading:'Article One',
     date: 'Aug 10, 2017',
