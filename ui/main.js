921b7ca4d7778b705c73dc2aa8309ef22f82ed8b
manu0309 <guptamanu530@gmail.com>
2017-08-12 13:32:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,4 +2,13 @@ console.log('Loaded!');
 
 var element = document.getElementById('main-text');
 
-element.innerHTML = 'New value'
+element.innerHTML = 'New value';
+
+var img = document.getElementById('img');
+
+img.onclick = function(){
+  
+  img.style.marginLeft = '100px';  
+};
+
+
