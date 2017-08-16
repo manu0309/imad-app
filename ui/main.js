e562a52d7293997f63ac61e93e3d378c9b5b4e78
manu0309 <guptamanu530@gmail.com>
2017-08-16 18:58:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -18,8 +18,8 @@ button.onclick = function () {
         }
     };
 
-    request.open('GET','http://guptamanu530.imad.hasura-app.io/counter',true);
-    request.send(null); 
+   // request.open('GET','http://guptamanu530.imad.hasura-app.io/counter',true);
+//    request.send(null); 
     
 
     
