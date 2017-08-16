01e3420186506b154539c30f43e09261133e91d6
manu0309 <guptamanu530@gmail.com>
2017-08-16 20:35:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -18,9 +18,9 @@ button.onclick = function () {
         }
     };
 
-    request.open('GET','http://guptamanu530.imad.hasura-app.io/counter',true);
+  request.open('GET','http://guptamanu530.imad.hasura-app.io/counter',true);
     request.send(null); 
-    
+      
 
     
 };
@@ -31,17 +31,32 @@ var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
     
+     var request = new XMLHttpRequest();
     
     
+    //Capture the response and store it in a variable
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            if (request.status === 200){
+  
     
-    
-    var names = ['name1','name2','name3','name4'];
+   var names = request.responseText;
+   names = JSON.parse(names);
     var list = '';
     for(var i=0; i<names.length;i++){
         list += '<li>' + names[i] +'</li>';
         
     }
+            
     var ul = document.getElementById('namelist');
     ul.innerHTML = list;
+    
+            }
+        }
+    };
+    
+  request.open('GET','http://guptamanu530.imad.hasura-app.io/submit-name?name=' + name,true);
+    request.send(null); 
+    
     
 };
