4fe61d4585d7a4da60c08c2133359dfce1b0da30
manu0309 <guptamanu530@gmail.com>
2017-08-16 20:25:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -110,9 +110,9 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 var names = [];
-app.get('/submit-name/:name',function(req,res){
+app.get('/submit-name',function(req,res){
     
-    var name = req.params.name;
+    var name = req.query.name;
     names.push(name);
     
     res.send(JSON.stringify(names));
