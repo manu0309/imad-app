d3468b82584583bc238cafa3081da9063df8de84
manu0309 <guptamanu530@gmail.com>
2017-08-21 12:42:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -102,7 +102,7 @@ function hash (input,salt){
     return hashed.toString('hex');
 }
 
-app.get('/hash/:input',function(res,req){
+app.get('/hash/:input',function(req,res){
     var hashedString = hash(req.params.input,'this-is-some-random-string');
     res.send(hashedString);
 });
