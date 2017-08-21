54294179082842ea5b6a6fbfeff3cab4305fb3dd
manu0309 <guptamanu530@gmail.com>
2017-08-21 12:38:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool  = require('pg').Pool;
+var crypto = require('crypto');
 
 var config = {
     user: 'guptamanu530',
@@ -93,6 +94,17 @@ return htmlTemplate;
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+
+function hash (input,salt){
+    var hashed = crypto.pbkdf25sync(input,salt,10000,512,'sha512');
+    return hashed.toString('hex');
+}
+
+app.get('/hash/:input',function(res,req){
+    var hashedString = hash(req.params.input,'this-is-some-random-string');
+    res.send(hashedString);
 });
 
 var pool  = new Pool(config);
